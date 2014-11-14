e0d5c3d45bee8d568ab7f01b2365ee7f4e6a1d79
Hans-Peter Stoerr <http://www.stoerr.net/>
2014-11-14 16:37:41 +0100

    intro index mapping functions

--- src/main/scala/net/stoerr/stocklearning/nnfunction/NNasFunction.scala
+++ src/main/scala/net/stoerr/stocklearning/nnfunction/NNasFunction.scala
@@ -23,21 +23,38 @@ trait DoubleFunctionWithGradient extends (Array[Double] => Double) {
  */
 class NNasFunction(inputSize: Int, hiddenSize: Int, outputSize: Int) {
 
-  /** Dimension of the weight vector */
-  val dimension = hiddenSize * (inputSize + 1) + outputSize * (hiddenSize + 1)
+  /** Dimension of the weight vector.
+    * w1_jk (in_k -> o1_j) : (inputSize + 1)*j + k
+    * offset1_j (-> o1_j) : (inputSize + 1)*j + inputSize
+    * w2_ij (o1_j -> o2_i) : (inputSize + 1) * hiddenSize + (hiddenSize + 1) * i + j
+    * offset2_i (-> o2_i) : (inputSize + 1) * hiddenSize + (hiddenSize + 1) * i + hiddenSize
+    */
+  val dimension = (inputSize + 1) * hiddenSize + (hiddenSize + 1) * outputSize
+
+  private def ind_w1(j: Int, k: Int) = (inputSize + 1) * j + k
+
+  private def ind_offset1(j: Int) = ind_w1(j, inputSize)
+
+  private def ind_w2(i: Int, j: Int) = (inputSize + 1) * hiddenSize + (hiddenSize + 1) * i + j
+
+  private def ind_offset2(i: Int) = ind_w2(i, hiddenSize)
 
   private class Calculation(example: Example, weights: Array[Double]) {
     val hiddenOut: Array[Double] = Array.ofDim(hiddenSize)
-    for (i <- 0.until(hiddenSize)) hiddenOut(i) = dotProductAndTanh(inputSize, example.inputs, 0, weights, (inputSize + 1) * i)
+    for (i <- 0.until(hiddenSize)) hiddenOut(i) = dotProductAndTanh(inputSize, example.inputs, 0, weights, ind_w1(i, 0))
     val out: Array[Double] = Array.ofDim(outputSize)
-    for (i <- 0.until(outputSize)) out(i) = dotProductAndTanh(hiddenSize, hiddenOut, 0, weights,
-      (inputSize + 1) * hiddenSize + (hiddenSize + 1) * i)
+    for (i <- 0.until(outputSize)) out(i) = dotProductAndTanh(hiddenSize, hiddenOut, 0, weights, ind_w2(i, 0))
   }
 
   private class DerivCalculation(example: Example, weights: Array[Double]) extends Calculation(example, weights) {
     val (gain, gainOut2Gradient) = example.gainWithGradient(out)
     val gradient: Array[Double] = Array.ofDim(dimension)
-    // XXX
+    for ((o2i, i) <- out.zipWithIndex) {
+      val o2id = (1 - o2i * o2i) * gainOut2Gradient(i) // d(gain)/d(o2_i)
+      for ((o1, j) <- hiddenOut.zipWithIndex) {
+        // gradient((inputSize + 1) * hiddenSize + (hiddenSize + 1) * i))
+      }
+    }
     val result = (gain, gradient)
   }
 
