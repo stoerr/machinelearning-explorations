b8b9dd9517047b13361a604d7235854ffcc9588b
stoerr <KYaLm9tsZbTNcuUhOr8M>
2014-11-18 22:21:52 +0100

    term enumerator is a little less awkward.

--- src/test/scala/net/stoerr/stocklearning/common/TestAbstractTermEnumerator.scala
+++ src/test/scala/net/stoerr/stocklearning/common/TestAbstractTermEnumerator.scala
@@ -8,23 +8,30 @@ import org.scalatest.FunSuite
  */
 class TestAbstractTermEnumerator extends FunSuite {
 
-  object SimpleTermEnumerator extends TermEnumerator {
+  object SimpleTermEnumerator extends TermEnumerator[(Double, Double)] {
 
     type TwoVars = TermFunctionWithComplexity[(Double, Double), Double]
 
     val basic: Stream[TwoVars] = Stream(nullary("0.5", _ => 0.5), nullary("2", _ => 2),
       nullary("x", x => x._1), nullary("y", x => x._2))
 
-    def r : (TwoVars, TwoVars) => TwoVars = binary("+", _ + _)
-
     val terms: Stream[TwoVars] = basic #::: mergeStreams(
       terms map unary("-", -_),
       terms map unary("2 *", 2 * _),
-      binaryOpStream(r, terms, terms)
+      binaryOpStream[Double, Double, Double](terms, terms, "+", _ + _),
+      binaryOpStream[Double, Double, Double](terms, terms, "*", _ * _),
+      binaryOpStream[Double, Double, Double](terms, terms, "/", _ / _)
     )
 
   }
 
-  SimpleTermEnumerator.terms.take(50).foreach(println(_))
+  test("Find terms for simple function") {
+    val candidates = SimpleTermEnumerator.terms
+      .filter(t => t.func(2, 2) == 6)
+      .filter(t => t.func(1, 1) == 3)
+      .filter(t => t.func(2, 1) == 5)
 
+    candidates.take(10).foreach(println)
+    assert(candidates.take(10).size == 10)
+  }
 }
