07f7e859bcd2e2936bb8a28656c44ae03df36339
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2018-09-30 19:46:08 +0200

    Stepper created

--- src/main/scala/net/stoerr/stocklearning/common/NumericalMinimumFinder.scala
+++ src/main/scala/net/stoerr/stocklearning/common/NumericalMinimumFinder.scala
@@ -12,6 +12,14 @@ object NumericalMinimumFinder {
     else (x0 * x0 * (y1 - y2) + x1 * x1 * (y2 - y0) + x2 * x2 * (y0 - y1)) / 2 /
       (x0 * y1 - x0 * y2 - x1 * y0 + x1 * y2 + x2 * y0 - x2 * y1)
 
+  def derivatives(x0: Double, y0: Double, x1: Double, y1: Double, x2: Double, y2: Double): (Double, Double) = {
+    def taylor(xa: Double, ya: Double, xb: Double, yb: Double) =
+      ((xa * xa * yb - xb * xb * ya) / (xa * xb * (xa - xb)), 2 * (-xa * yb + xb * ya) / (xa * xb * (xa - xb)))
+
+    taylor(x1 - x0, y1 - y0, x2 - x0, y2 - y0)
+  }
+
+
   /** Limits #interpolatedMinimum to maxstep times the spread of the x values. */
   def interpolatedMinimumStep(f: Double => Double, xy0: (Double, Double), xy1: (Double, Double), xy2: (Double, Double), maxstep: Double = 4): (Double, Double) = {
     val Array((x0, y0), (x1, y1), (x2, y2)) = Array(xy0, xy1, xy2).sortBy(_._1)
@@ -51,6 +59,92 @@ object NumericalMinimumFinder {
       // println(List(xy0, xy1, xy2))
     }
     Array(xy0, xy1, xy2).sortBy(_._2).apply(0)
+  }
+
+  /** Search for minimum in several steps; we start with x=0,1,2 */
+  def secondorderMinimumSearch2(f: Double => Double, maxstep: Double = 16, maxiter: Int = 5, eps: Double = 1e-6): (Double, Double) = {
+    val stepper = Stepper((0.0, f(0.0)), (1.0, f(1.0)), (2.0, f(2.0)), f)
+    for (_ <- 0 until maxiter if (stepper.ymax - stepper.ymin > eps)) {
+      stepper.step()
+    }
+    stepper.xy0
+  }
+
+}
+
+/** We estimate the point of the minimum by either taking the 2nd order interpolation in case of valleys,
+  * or stepping outside the interval in the case of hills. If we are outside the interval, and we actually went up the slope, we go a part of the last step, instead.
+  * In the case of valleys, we want to use the 2nd order interpolation unless it results in a higher f. Otherwise use downhill, too.
+  * xy0 is last location, xy1 after previous step, xy2 two steps before. */
+case class Stepper(var xy0: (Double, Double), var xy1: (Double, Double), var xy2: (Double, Double), f: Double => Double) {
+  def x0: Double = xy0._1
+
+  def y0: Double = xy0._2
+
+  def x1: Double = xy1._1
+
+  def y1: Double = xy1._2
+
+  def x2: Double = xy2._1
+
+  def y2: Double = xy2._2
+
+  def xmin: Double = Array(x0, x1, x2).min
+
+  def xmax: Double = Array(x0, x1, x2).max
+
+  def ymax: Double = Array(y0, y1, y2).max
+
+  def ymin: Double = Array(y0, y1, y2).min
+
+  override def toString: String = Array(xy0, xy1, xy2).mkString(" ")
+
+  /** Approximation of first derivative at x0 */
+  def d1: Double = NumericalMinimumFinder.derivatives(x0, y0, x1, y1, x2, y2)._1
+
+  /** Approximation of second derivative at x0 */
+  def d2: Double = NumericalMinimumFinder.derivatives(x0, y0, x1, y1, x2, y2)._2
+
+  def valley: Boolean = d2 > 0
+
+  private def outside(x: Double) = (x - x0) * (x2 - x) < 0
+
+  private def insequence(xa: Double, xb: Double, xc: Double) = (xa - xb) * (xb - xc) > 0
+
+  private val maxstep = Math.pow(Math.E, Math.PI)
+
+  def step(): Unit = {
+    val xyn = xynext()
+    val Array(xya, xyb) = Array(xy0, xy1).sortBy(-_._2) // ya >= yb
+    xy2 = xya
+    xy1 = xyb
+    xy0 = xyn
+  }
+
+  def xynext(): (Double, Double) = {
+    println(this)
+    if (valley) {
+      var xest = NumericalMinimumFinder.interpolatedMinimum(x0, y0, x1, y1, x2, y2)
+      xest = max(xest, xmin - maxstep * (xmax - xmin))
+      xest = Math.min(xest, xmax + maxstep * (xmax - xmin))
+      var yest = f(xest)
+      if (yest < max(y0, y1)) {
+        println("interpol -> " + xest)
+        return (xest, yest)
+      } else {
+        println("interpol not - " + (xest, yest))
+      }
+    }
+    val Array((xa, ya), (xb, yb)) = Array(xy0, xy1).sortBy(-_._2) // ya >= yb
+    var xest = xb + (xb - xa) * (Math.E - 1)
+    var yest = f(xest)
+    if (yest < yb) {
+      println("bigstep -> " + xest)
+      return (xest, yest)
+    }
+    xest = xb + (xb - xa) / (Math.PI - 1)
+    println("smallstep -> " + xest)
+    (xest, f(xest))
   }
 
 }
